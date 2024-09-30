05c0c483fd1e22e34d1e99d7e2f84edb19ddbd13
MisterNoNameLP <misternonamelp@web.de>
2024-09-30 14:49:17 +0200

    v10.0.37_prep

--- .release/testpack-v10.0.36_Universal/install-modpack_v10.0.36.bat
+++ .release/testpack-v10.0.36_Universal/install-modpack_v10.0.36.bat
@@ -1,2 +1,2 @@
 @echo off
-java -jar packwiz-installer-bootstrap.jar -s client https://github.com/OpenPlayVerse/testpack/raw/d2640d535d3237dc8346c892e4e04837cede73c7/packwiz/pack.toml
+java -jar packwiz-installer-bootstrap.jar -s client https://github.com/OpenPlayVerse/testpack/raw/50a0b9d41d4c1e6e274bec220006541fb1526168/packwiz/pack.toml
